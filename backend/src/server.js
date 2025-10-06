f2a37bd91cc503868ad3608d3c1e9f7e8758d9da
quanpva62 <phamquan6023@gmail.com>
2025-10-07 04:00:07 +0700

    deploying

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,31 @@ import { connect } from "mongoose";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const _dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173" })
+  );
+}
 
 app.use("/api/tasks", taskRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
